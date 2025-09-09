1ee590004276ff29f6dc719fbbcac8b45b029fdb
bongiBong-create <mairanss543@inbox.ru>
2025-09-09 20:07:26 +0300

    done soundcloud player

---
+++ index.js
@@ -0,0 +1,20 @@
+import WaveSurfer from "https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js";
+
+const wavesurfer = WaveSurfer.create({
+  container: document.body,
+  waveColor: "rgb(200, 0, 200)",
+  progressColor: "rgb(100, 0, 100)",
+  url: "/audio/POPARRANGE116DMINOR[DEMO].mp3",
+});
+
+document.querySelectorAll("button").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const src = btn.dataset.src; // берём путь из data-src
+    wavesurfer.load(src); // загружаем новый трек
+    wavesurfer.on("ready", () => wavesurfer.play()); // автостарт при готовности
+  });
+});
+
+wavesurfer.on("click", () => {
+  wavesurfer.play();
+});
